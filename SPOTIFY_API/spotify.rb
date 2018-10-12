8b269960cb4f0172d0e89681460f6d1a548fd6f9
Fredis <fredericstojicevic@gmail.com>
2018-10-12 17:49:11 +0200

    Spotify API.

--- SPOTIFY_API/spotify.rb
+++ SPOTIFY_API/spotify.rb
@@ -1,5 +1,38 @@
 require 'dotenv'
+require 'base64'
+require 'httparty'
 
 Dotenv.load
 
-puts ENV['SPOTIFY_KEY']
+def encode
+
+	chain =  ENV['SPOTIFY_KEY'] + ":" + ENV['SPOTIFY_SECRET_KEY']
+	encoded = Base64.encode64(chain).delete("\n")
+end
+
+@token_array = my_post_request = HTTParty.post(
+	"https://accounts.spotify.com/api/token", 
+  headers: {Authorization: "Basic #{encode}"}, 
+  body: {
+  		grant_type: "client_credentials"
+  	}
+	)
+
+@token = @token_array["access_token"]
+
+def get_latest_release
+	
+	@headers = {
+		"Content-Type": 'application/json',
+  	"Accept": 'application/json',
+  	"Authorization": "Bearer #{@token}"
+	}
+
+	my_get_request = HTTParty.get(
+		"https://api.spotify.com/v1/browse/new-releases?limit=2",
+	  headers: @headers
+		)
+
+end
+
+puts get_latest_release
